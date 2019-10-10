47ec8abd3e187e2cc62f21e18f3c476478fb4df2
kangminsooKMS <48310120+kangminsooKMS@users.noreply.github.com>
2019-10-10 16:33:07 +0900

    Add files via upload

---
+++ AR02_Rpt03/AR02_2led/AR02_2led.ino
@@ -0,0 +1,62 @@
+/*
+ 예제 4.1
+ LED 점멸
+*/
+
+const int ledA   =  1;
+const int ledB   =  2;
+const int ledC   =  4;
+const int ledD   =  8;
+
+int number = 1;
+boolean flag = true;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledA, OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(ledC, OUTPUT);
+  pinMode(ledD, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledA,HIGH);
+  digitalWrite(ledB,LOW);
+  digitalWrite(ledC,LOW);
+  digitalWrite(ledD,LOW);
+  delay(100 * number);
+  digitalWrite(ledA,LOW);
+  digitalWrite(ledB,HIGH);
+  digitalWrite(ledC,LOW);
+  digitalWrite(ledD,LOW);
+  delay(100 * number);
+  digitalWrite(ledA,LOW);
+  digitalWrite(ledB,LOW);
+  digitalWrite(ledC,HIGH);
+  digitalWrite(ledD,LOW);
+  delay(100 * number);
+  digitalWrite(ledA,LOW);
+  digitalWrite(ledB,LOW);
+  digitalWrite(ledC,LOW);
+  digitalWrite(ledD,HIGH);
+  Serial.print("delay = ");
+  Serial.print(100 * number);
+  Serial.println(" msec");
+  delay(100 * number);
+
+if(flag) {
+  number++;
+} else {
+  number--;
+}
+
+
+if(number >= 20){
+    flag = false;
+  }
+else if(number == 1){
+    flag = true;
+  }
+}
